d34a79d41c0b0c094273c4940fb66b2dd737a4a3
Will Yazdani <yazd0006@gmail.com>
2022-01-13 23:31:48 -0600

    added character check section

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,57 @@
 //password characters
-var letters = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "123456789";
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var numeric = "123456789";
 var special = "!@#$%^&*()-=_+[]{}~`<>,./?|:;";
 
+//generator
 var generatePassword = function() {
 
+  //length prompts
   var length = "";
 
   while (length === "" || length === null) {
     var passLength = prompt("How long would you like your password to be? Must be 8-18 characters.");
 
     if (isNaN(passLength)) {
-      window.alert("Value entered must be a number");
+      window.alert("Please enter value between 8-128.");
     } else if (passLength < 8) {
-      window.alert("Password length must be between 8-128 characters.");
+      window.alert("Please enter value between 8-128.");
     } else if (passLength > 128) {
-      window.alert("Password length must be between 8-128 characters.");
+      window.alert("Please enter value between 8-128.");
     } else if (passLength === null) {
       window.alert("Please enter value between 8-128.");
     } else {
       length = passLength;
     }
   }
+
+  //lowercase confirm
+  var lowerCase = window.confirm("Click 'OK' if you would like your password to contain lowercase characters.");
+
+  //uppercase confirm
+  var upperCase = window.confirm("Click 'OK' if you would like your password to contain uppercase characters.");
+
+  //numeric confirm
+  var numericChars = window.confirm("Click 'OK' if you would like your password to contain numeric characters.");
+
+  //special confirm
+  var specialChars = window.confirm("Click 'OK' if you would like your password to contain special characters.");
+
+};
+
+//character check
+var passOutput = "";
+if (lowerCase) {
+  passOutput += alphabet.toLowerCase();
+}
+if (upperCase) {
+  passOutput += alphabet.toUpperCase();
+}
+if (numericChars) {
+  passOutput += numeric;
+}
+if (specialChars) {
+  passOutput += special;
 }
 
 
