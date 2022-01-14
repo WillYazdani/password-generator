571cf225aef4bebb83ee4184363d244a0df58725
Will Yazdani <yazd0006@gmail.com>
2022-01-13 21:56:12 -0600

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,23 @@
+//password characters
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "123456789";
+var special = "!@#$%^&*()-=_+[]{}~`<>,./?|";
+
+var generatePassword = function
+
+
+
+
+
+
+//button generates password prompt
+
+//prompt for criteria
+
+//prompt for length 8-128
+
+//prompt for character types - lowercase, uppercase, numeric, special characters
+
+//when prompts are answered then password is generated matches selected criteria
+
+//when password is generated then it is displayed in an alert or on the page
